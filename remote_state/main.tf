14e6c12f73fe080d28e663341275ca55bab2f41e
George Torianik <htorianik@amplify.com>
2022-05-09 20:01:55 +0300

    Add remote state infrastrcture.

---
+++ remote_state/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state--testing"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state_pab" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls   = true
+  block_public_policy = true
+}
+
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-state-lock"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
